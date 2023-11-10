a4998afc1eceaf3ee0ed047fd3fea3ebf779cb9e
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2023-11-10 01:20:48 -0600

    idk make this shit public?

--- Runtime/JsonConverters/FlatVector3Converter.cs
+++ Runtime/JsonConverters/FlatVector3Converter.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace PoseClipboard
 {
-    internal class FlatVector3Converter : JsonConverter<Vector3>
+    public class FlatVector3Converter : JsonConverter<Vector3>
     {
         public override Vector3 ReadJson(JsonReader reader, Type objectType, Vector3 existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
